ac8b1368faa0841d0d6b721a060d7e07946da475
Patrick Kennedy <Dysiode@gmail.com>
2009-12-27 13:18:30 -0700

    Updated __contains__'s checks to be strict inequalities
    This solved pretty much every problem with exclusions ever.

--- structs.py
+++ structs.py
@@ -13,7 +13,7 @@ Point.__gt__ = lambda x, y: x.x > y.x and x.y > y.y
 Point.__le__ = lambda x, y: x.x <= y.x and x.y <= y.y
 Point.__lt__ = lambda x, y: x.x < y.x and x.y < y.y
 Point.floor = lambda self: Point(int(self.x), int(self.y))
-	
+
 class Rect(object):
 	def __init__(self, left, top, width, height, absolute=False):
 		self.left = left
@@ -24,25 +24,25 @@ class Rect(object):
 		else:
 			self.width = width
 			self.height = height
-		
+
 	def __repr__(self):
 		return "Rect(%s, %s, %s, %s)" % (self.left, self.top, self.right, self.bottom)
-		
+
 	def __contains__(self, other):
 		if isinstance(other, Point):
-			return self.left <= other.x <= self.right \
-				and self.top <= other.y <= self.bottom
-		return self.right	>= other.left \
-			and self.left	<= other.right \
-			and self.bottom	>= other.top \
-			and self.top	<= other.bottom
-		
+			return self.left < other.x < self.right \
+				and self.top < other.y < self.bottom
+		return self.right	> other.left \
+			and self.left	< other.right \
+			and self.bottom	> other.top \
+			and self.top	< other.bottom
+
 	def __gt__(self, other):
 		return self.left	< other.left \
 			and self.top	< other.top \
 			and self.right	> other.right \
 			and self.bottom	> other.bottom
-	
+
 	def __ge__(self, other):
 		return self.left	<= other.left \
 			and self.top	<= other.top \
@@ -54,19 +54,19 @@ class Rect(object):
 			and self.top	== other.top \
 			and self.right	== other.right \
 			and self.bottom	== other.bottom
-		
+
 	def __lt__(self, other):
 		return self.left	> other.left \
 			and self.top	> other.top \
 			and self.right	< other.right \
 			and self.bottom	< other.bottom
-		
+
 	def __le__(self, other):
 		return self.left	>= other.left \
 			and self.top	>= other.top \
 			and self.right	<= other.right \
 			and self.bottom	<= other.bottom
-	
+
 	def __add__(self, other):
 		return Rect(self.left+other.left, self.top+other.top,
 					self.right+other.right, self.bottom+other.bottom)
@@ -93,15 +93,15 @@ class Rect(object):
 		if self.right > cx and self.bottom > cy: matched.append(2)
 		if self.left < cx and self.bottom > cy: matched.append(3)
 		return matched
-	
+
 	def copy(self):
 		return Rect(self.left, self.top, self.width, self.height)
-		
+
 	def fracture(self, point):
 		"""Fracture self about point and return the results
-		
+
 		Rects that aren't fractured are filled with None
-		
+
 		"""
 		logging.debug("Fracturing %s about %s" % (self, point))
 		# We assume the point is the origin of another rect.
@@ -131,14 +131,14 @@ class Rect(object):
 							 min(self.right, px),
 							 self.bottom,
 							 absolute=True)
-		#print "Resulting Shards", shards 
+		#print "Resulting Shards", shards
 		return shards
-			
-			
+
+
 	def get_bottom(self): return self.top+self.height
 	def set_bottom(self, v): self.height = v-self.top
 	bottom = property(get_bottom, set_bottom)
-	
+
 	def get_right(self): return self.left+self.width
 	def set_right(self, v): self.width = v-self.left
 	right = property(get_right, set_right)
@@ -150,4 +150,3 @@ class Rect(object):
 	def ul(self): return Point(self.left, self.top)
 	@property
 	def lr(self): return Point(self.right, self.bottom)
-		
